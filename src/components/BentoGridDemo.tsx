167e96f57799ddffae1ffb9511b1def3148ed055
NuReach <nureach46@gmail.com>
2024-06-21 22:41:37 +0700

    add photo

--- src/components/BentoGridDemo.tsx
+++ src/components/BentoGridDemo.tsx
@@ -2,6 +2,7 @@ import { cn } from "../lib/utils";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./bento-grid";
 import { LucideOctagonX } from "lucide-react";
+import Image from "next/image";
 
 
 export function BentoGridDemo() {
@@ -27,74 +28,74 @@ const items = [
   {
     title: "Bali Villa Kompot",
     description: "Explore the modern of web solution ideas and inventions.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/bali1.png`} alt="bali" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Note taking with AI",
     description: "Dive into the ai technology to store all your note.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/note1.png`} alt="note" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "E-Commerce Online Store",
     description: "Browse our curated selection of high-quality products and discover your new favorites today",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/ecommerce1.png`} alt="ecommerce" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "My Portfolio website",
     description:
       "Welcome to my portfolio, showcasing my skills and experience to bring your vision to life.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={100} src={`/portfo.png`} alt="myportfo" className=" h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Hotel Management System",
     description: "Streamline your hotel operations with a comprehensive system for reservations, guest management, and efficient daily tasks.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/no-image.png`} alt="hotel" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Limkokwing Online Quiz",
     description: "Test your knowledge and prepare for success with Limkokwing's online quizzes.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/lkw1.png`} alt="lkw" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Rebuild Apple 3D Website",
     description: "Immerse yourself in interactive experiences with eye-catching 3D website designs.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={100} src={`/apple1.png`} alt="apple" className=" h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Video Streaming ",
     description: "Lose yourself in a world of entertainment with endless video streaming options.",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/vdostream1.png`} alt="vdo" className=" h-full object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Cambodia Scholarship Center",
     description: "Storing all data in cloud",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/cscdata1.png`} alt="csc" className="  h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Cambodia Scholarship Center E-Learning",
     description: "Enhance scholarship rate with our cousrse",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/csc1.png`} alt="csc" className="  h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "E-Menu for your business",
     description: "Choose the best menu for your customer",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/no-image.png`} alt="e-menu" className="  h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   {
     title: "Mobile Legend Drafting",
     description: "Analyze draft winrate with our system",
-    header: <Skeleton />,
+    header: <Image width={1200} height={900} src={`/mlbb1.png`} alt="mlbb" className="  h-[270px] object-cover" />,
     icon: <LucideOctagonX />,
   },
   
